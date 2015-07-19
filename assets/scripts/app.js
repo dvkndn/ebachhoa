08e6f8b5f8ceb130399439a0ecfea9b9c5d5c667
Thien Do <dvkndn@gmail.com>
2015-07-20 02:18:30 +0700

    Add category-tree

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,23 @@
 $(function() {
 	// tab handler
-	$(".tabs").on("click", ".tabs__title", function(event) {
+	$(".tabs").on("click", "> .tabs__title", function(event) {
 		// remove all sibling class
 		$(event.target).siblings().removeClass("selected");
 		// add class
 		$(event.target).addClass("selected");
+	});
+
+	// accordion handler
+	$(".accordion").on("click", "> .accordion__title", function(event) {
+		var activeClass = "accordion--selected";
+		// remove all sibling class
+		$(event.currentTarget).parent().siblings().removeClass(activeClass);
+		// add class
+		$(event.currentTarget).parent().addClass(activeClass);
+	});
+
+	// disable a #
+	$("a[href=\"#\"]").on("click", function(event){
+		event.preventDefault();
 	});
 });
